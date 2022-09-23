29dc0b4e0a71480c56cfd31a6ead76e36607274a
Jonhny <jonhny.ha.nguyen@gmail.com>
2022-09-23 10:30:47 -0500

    added some js for task 5

--- JS/itemsController.js
+++ JS/itemsController.js
@@ -1,20 +1,28 @@
-class itemController {
-  constructor(currentId = 0) {
-    this.items = [];
-    this.currentId = currentId;
+class ItemsController{
+  constructor(currentId=0){
+      this.items=[];
+      this.currentId = currentId;
   }
 
-  addItem(name, description, img) {
-    const product1 = {
-      id: this.currentId++,
-      name: name,
-      description: description,
-      img: img
-    };
-    this.items.push(product1);
+  addItem(name,description,img){
+      const product1 = {
+          id: this.currentId++,
+          name: name,
+          description: description,
+          img: img
+      };
+      this.items.push(product1);
   }
-};
+  loadItemsFromLocalStorage() {
+    const storageItems = localStorage.getItem("items")
+    if (storageItems) {
+        const items = JSON.parse(storageItems)
+        for (var i = 0, size = items.length; i < size; i++) {
+            const item = items[i];
+            this.items.push(item);
+        }
+    }
+}
+}
 
-let item = new itemController();
-item.addItem("bloop beep");
-console.log(item); 
+
